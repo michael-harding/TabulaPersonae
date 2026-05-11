82283d9a8bb463e4ee3436a9d57ebbf86f0d853b
Michael Harding <2816319+michael-harding@users.noreply.github.com>
2026-05-11 10:21:13 -0400

    move firebase secrets to .env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCU7Qs0jZNnFjecByDfTSah12Mp4ePLh8A",
-  authDomain: "dnd-character-803e4.firebaseapp.com",
-  projectId: "dnd-character-803e4",
-  storageBucket: "dnd-character-803e4.firebasestorage.app",
-  messagingSenderId: "264209658611",
-  appId: "1:264209658611:web:51f1d4ae4ca26e1944fd86"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
